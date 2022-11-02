0bc507a070bf71eb7501e8e29b265334df0546d2
OstrovskayAngelina <114667213+OstrovskayAngelina@users.noreply.github.com>
2022-11-02 22:08:09 +0300

    Add files via upload

---
+++ atest.py
@@ -0,0 +1,4 @@
+qtyOranges = int(input("Ангелина сколько апельсинов вы желаете? "))
+priceOrange = float(input("Цена одного апельсина? "))
+sumOranges = qtyOranges * priceOrange
+print("Ты заплатишь", sumOranges, "руб.")
